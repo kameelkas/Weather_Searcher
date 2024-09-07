51491931e31b489ebb0354c918f9f972d4f8dce7
Kameel Kasumu <kameelkas@gmail.com>
2024-09-07 14:12:29 -0600

    Code Restructure Continuing -> Debugging

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
  <?php
-error_reporting(0);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 // initiaiting variables that will be used later on
